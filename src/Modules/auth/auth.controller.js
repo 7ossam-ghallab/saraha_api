90f110d80a7bedf1c187924ce19851e23b0febe5
7ossam-ghallab <7ossam.ghallab@gmail.com>
2025-02-07 20:06:05 +0200

    v-6

--- src/Modules/auth/auth.controller.js
+++ src/Modules/auth/auth.controller.js
@@ -1,9 +1,11 @@
 import {Router} from 'express'
 import * as authServices from './services/auth.services.js'
+import validationMiddleware from '../../Middlewares/validation.middleware.js'
+import signUpValidationSchema from '../../validators/signUp.schema.js'
 
 const authRouter = Router()
 
-authRouter.post('/signup', authServices.signUp)
+authRouter.post('/signup', validationMiddleware(signUpValidationSchema),authServices.signUp)
 authRouter.post('/signin', authServices.signIn)
 authRouter.post('/logout', authServices.logOut)
 authRouter.get('/verify-email/:token', authServices.verifyEmail)

4b9046275d826d1add2d0acf0f012d5e0c88169a
7ossam-ghallab <7ossam.ghallab@gmail.com>
2025-02-07 20:06:05 +0200

    fix(validation): enforce Joi validation on signup route

--- src/Middlewares/validation.middleware.js
+++ src/Middlewares/validation.middleware.js
@@ -1,11 +1,20 @@
-export const validationMiddleware = (schema) => {
+const validationMiddleware = (schema) => {
   return (req, res, next) => {
     const schemaKeys = Object.keys(schema)
 
+    let validationErrors = []
     for (const key of schemaKeys) {
-      const result = schema[key].validate(req[key]);
-      console.log(result)
+      const {error} = schema[key].validate(req[key], {abortEarly : false}); // {abortEarly : false} to show all errors not the first
+      // console.log(error)
+      if (error) {
+        validationErrors.push(...error.details);
+      }
+    }
+    if (validationErrors.length) {
+      return res.status(400).json({message : "validation Errors", errors: validationErrors });
     }
     next();
   }
 }
+
+export default validationMiddleware;
--- src/validators/signUp.schema.js
+++ src/validators/signUp.schema.js
@@ -1,6 +1,6 @@
-import Joi from "joi"
+import Joi from "joi";
 const signUpValidationSchema = {
-  body : joi.object({
+  body : Joi.object({
     username : Joi.string().min(3).max(20).required(),
     email : Joi.string().email().required(),
     password : Joi.string().min(8).required(),
